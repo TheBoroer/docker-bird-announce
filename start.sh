ce11e7979a8b2531dbfeae799580bb0b5153059b
Boro Vukovic <boromobile@gmail.com>
2018-03-24 03:14:48 -0400

    WIP

--- start.sh
+++ start.sh
@@ -8,11 +8,11 @@
 yes | cp -rf /etc/bird/bird6.template.conf /etc/bird/bird6.conf
 
 # Setup IPv6 Bird
-sed "s~_____ROUTER_ID_____~$ROUTER_ID~g" /etc/bird/bird6.conf
-sed "s~_____ROUTER_ASN_____~$ROUTER_ASN~g" /etc/bird/bird6.conf
-sed "s~_____ROUTER_IPV6_____~$ROUTER_IPV6~g" /etc/bird/bird6.conf
-sed "s~_____ANNOUNCE_IPV6_____~$ANNOUNCE_IPV6~g" /etc/bird/bird6.conf
-sed "s~_____PEER_ASN_____~$PEER_ASN~g" /etc/bird/bird6.conf
-sed "s~_____PEER_IPV6_____~$PEER_IPV6~g" /etc/bird/bird6.conf
+sed -i "s~_____ROUTER_ID_____~$ROUTER_ID~g" /etc/bird/bird6.conf
+sed -i "s~_____ROUTER_ASN_____~$ROUTER_ASN~g" /etc/bird/bird6.conf
+sed -i "s~_____ROUTER_IPV6_____~$ROUTER_IPV6~g" /etc/bird/bird6.conf
+sed -i "s~_____ANNOUNCE_IPV6_____~$ANNOUNCE_IPV6~g" /etc/bird/bird6.conf
+sed -i "s~_____PEER_ASN_____~$PEER_ASN~g" /etc/bird/bird6.conf
+sed -i "s~_____PEER_IPV6_____~$PEER_IPV6~g" /etc/bird/bird6.conf
 
 /usr/sbin/bird6 -f -u bird -g bird
