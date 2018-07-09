2a9fad5cc8b9c08e6c22e68369095367f5e39ede
Boro Vukovic <TheBoroer@users.noreply.github.com>
2018-07-09 01:43:05 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,47 @@
 #!/bin/sh
 
-# Check if vars set
-#if [ ! -z "$GIT_EMAIL" ]; then
-#  
-#fi
+if [ ! -z "$CONFIG_BIRD" ]; then
+  # Setup IPv4 BIRD
+  
+  rm -rf /etc/bird/bird.conf
+  yes | cp -rf /etc/bird/bird.template.conf /etc/bird/bird.conf
+  
+  sed -i "s~_____ROUTER_ID_____~$ROUTER_ID~g" /etc/bird/bird.conf
+  sed -i "s~_____ROUTER_ASN_____~$ROUTER_ASN~g" /etc/bird/bird.conf
+  sed -i "s~_____ROUTER_IPV4_____~$ROUTER_IPV4~g" /etc/bird/bird.conf
+  sed -i "s~_____ANNOUNCE_IPV4_____~$ANNOUNCE_IPV4~g" /etc/bird/bird.conf
+  sed -i "s~_____PEER_ASN_____~$PEER_ASN~g" /etc/bird/bird.conf
+  sed -i "s~_____PEER_IPV4_____~$PEER_IPV4~g" /etc/bird/bird.conf
+fi
 
-rm -rf /etc/bird/bird6.conf
-yes | cp -rf /etc/bird/bird6.template.conf /etc/bird/bird6.conf
+if [ ! -z "$CONFIG_BIRD6" ]; then
+  # Setup IPv6 BIRD
+  
+  rm -rf /etc/bird/bird6.conf
+  yes | cp -rf /etc/bird/bird6.template.conf /etc/bird/bird6.conf
+  
+  sed -i "s~_____ROUTER_ID_____~$ROUTER_ID~g" /etc/bird/bird6.conf
+  sed -i "s~_____ROUTER_ASN_____~$ROUTER_ASN~g" /etc/bird/bird6.conf
+  sed -i "s~_____ROUTER_IPV6_____~$ROUTER_IPV6~g" /etc/bird/bird6.conf
+  sed -i "s~_____ANNOUNCE_IPV6_____~$ANNOUNCE_IPV6~g" /etc/bird/bird6.conf
+  sed -i "s~_____PEER_ASN_____~$PEER_ASN~g" /etc/bird/bird6.conf
+  sed -i "s~_____PEER_IPV6_____~$PEER_IPV6~g" /etc/bird/bird6.conf
+fi
 
-# Setup IPv6 Bird
-sed -i "s~_____ROUTER_ID_____~$ROUTER_ID~g" /etc/bird/bird6.conf
-sed -i "s~_____ROUTER_ASN_____~$ROUTER_ASN~g" /etc/bird/bird6.conf
-sed -i "s~_____ROUTER_IPV6_____~$ROUTER_IPV6~g" /etc/bird/bird6.conf
-sed -i "s~_____ANNOUNCE_IPV6_____~$ANNOUNCE_IPV6~g" /etc/bird/bird6.conf
-sed -i "s~_____PEER_ASN_____~$PEER_ASN~g" /etc/bird/bird6.conf
-sed -i "s~_____PEER_IPV6_____~$PEER_IPV6~g" /etc/bird/bird6.conf
 
-/usr/sbin/bird6 -f -u root -g root
+if [ ! -z $ENABLE_BIRD ] && [ ! -z $ENABLE_BIRD6 ]; then
+  # BIRD v4 and v6
+  /usr/sbin/bird6 -u root -g root
+  /usr/sbin/bird -f -u root -g root
+  
+elif [ ! -z $ENABLE_BIRD ]; then
+  # BIRD v4 only
+  /usr/sbin/bird -f -u root -g root
+  
+elif [ ! -z $ENABLE_BIRD6 ]; then
+  # BIRD v6 only
+  /usr/sbin/bird6 -f -u root -g root
+  
+else
+  echo "Note: BIRD and BIRD6 is disabled. Exiting."
+fi
